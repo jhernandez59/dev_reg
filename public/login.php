6d832fb4ad63faa4774c742af2b7420e948f3f0c
Jose Hernandez <jhernandez59@gmail.com>
2024-09-13 13:13:55 -0500

    start bluma branch

--- public/login.php
+++ public/login.php
@@ -29,37 +29,76 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+  />
 </head>
 <body>
+  <section class="container">
+    <div class="columns is-multiline">
+      <div class="column is-8 is-offset-2">
+        <!-- columnas arriba  -->
+        <div class="columns">
+          <div class="column left">
+              <h1 class="title is-1">Super Cool Website</h1>
+              <h2 class="subtitle colored is-4">Lorem ipsum dolor sit amet.</h2>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ex deleniti aliquam tempora libero excepturi vero soluta odio optio sed.</p>
+          </div>
 
-    <h3>Login</h3>
+          <div class="column right has-text-centered">
+            <h1 class="title is-4">Regístrate hoy</h1>
+            <p class="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
 
-    <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+            <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
         
-        <!-- email -->        
-        <div>
-        E-mail: <input type="text" name="email" value="<?=$validator ? $validator->getEmail() : '' ?>">
-        <span class="error">* <?= $errors['email'] ?? '' ?></span>
-        <br><br>
-        </div>
+                <!-- email -->        
+                <div class="field is-left">
+                  <label class="label has-text-left" for="email">Email</label>
+                  <div class="control">
+                    <input class="input is-medium" type="text" placeholder="Email" 
+                  name="email" value="<?=$validator ? $validator->getEmail() : '' ?>">
+                </div>
+                <p class="help is-danger has-text-left"><?= $errors['email'] ?? '' ?></p>                  
+                </div>
 
-        <!-- password -->
-        <div>
-        Password: <input type="password" name="password" value="<?=$validator ? $validator->getPassword() : '' ?>">
-        <span class="error"><?= $errors['password'] ?? '' ?></span>
-        <br>
-        </div>
-        
-        <br>
-        <a href="change_password.php">Cambiar contraseña</a>
-        <br><br>
+                <!-- password -->
+                <div class="field">
+                  <label class="label has-text-left" for="password">Contraseña</label>
+                  <div class="control">
+                  <input class="input is-medium" type="password" placeholder="Contraseña"
+                  name="password" value="<?=$validator ? $validator->getPassword() : '' ?>">
+                  <p class="help is-danger has-text-left"><?= $errors['password'] ?? '' ?></p>
+                  </div>
+                </div>
+                
+                <!-- submit -->
+                <div>
+                    <button class="button is-block is-primary is-fullwidth is-medium" type="submit">Entrar</button>
+                </div>
 
-        <!-- submit -->
-        <div>
-            <button type="submit">Entrar</button>
+                <br>
+                <a href="change_password.php">Cambiar contraseña</a>
+                <br><br>
+
+                <br>
+            </form>
+
+          </div>
         </div>
-        <br>
-    </form>
+      </div>
+      <!-- Fin columnas arriba -->
+
+      <div class="column is-8 is-offset-2">
+        <!-- columnas abajo -->
+        <div class="columns">
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
     
 <?php
 if (($_SERVER['REQUEST_METHOD'] == 'POST') && empty($errors)) {
@@ -88,11 +127,11 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && empty($errors)) {
     
 } else {
     // echo '<h3>Form Errors</h3>';
-    echo '<ul>';
-    foreach ($errors as $error) {
-        echo "<li>$error</li>";
-    }
-    echo '</ul>';
+    // echo '<ul>';
+    // foreach ($errors as $error) {
+    //    echo "<li>$error</li>";
+    // }
+    // echo '</ul>';
 }
 ?>
 
